9cafc392c2e5fbb3c3ec00ad4aced7ee288fb0e4
JDMino <jdminogomez@gmail.com>
2021-12-01 23:23:43 -0300

    primer commit modificado

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,4 +1,4 @@
-numero = int(input("Escriba un número positivo: "))
+numero = int(input("Escriba un número positivo por favor: "))
 while numero < 0:
     print("Ha escrito un número negativo")
     numero = int(input("Escriba un número positivo: "))
